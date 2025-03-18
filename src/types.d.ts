45b20f95a25594f553441e0c387355692801e534
gustadev24 <lgsc21211@gmail.com>
2025-03-17 20:10:24 -0500

    Add projects section

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+import CodeIcon from "@assets/icons/code.svg";
+
+export interface TagType {
+  icon?: typeof CodeIcon;
+  content: string;
+  class?: string;
+  iconClass?: string;
+}
+
+export interface ProjectCardType {
+  img: ImageMetadata;
+  title: string;
+  tags: TagType[];
+  description: string;
+  github: string;
+  preview?: string;
+}
