9882988f85aa36309e3f68a4274ebc9a91d9ab75
gustadev24 <lgsc21211@gmail.com>
2025-09-01 22:03:07 -0500

    feat: add two projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,6 +1,8 @@
-import ACMProjectImage from '@/assets/images/acm-project.webp';
-import ExoplanetsProjectImage from '@/assets/images/exoplanets-project.webp';
-import KnowledgeWardensProjectImage from '@/assets/images/knowledge-wardens-project.webp';
+import ACMImage from '@/assets/images/projects/acm.webp';
+import ExoplanetsImage from '@/assets/images/projects/exoplanets.webp';
+import KnowledgeWardensImage from '@/assets/images/projects/knowledge-wardens.webp';
+import TechartImage from '@/assets/images/projects/techart.webp';
+import ClassroomImage from '@/assets/images/projects/classroom.webp';
 import type { getI18n } from '@/i18n/index';
 import { genTags } from '@/lib/tags';
 import type { ProjectCardType } from '@/types';
@@ -11,9 +13,38 @@ export const genProjects: (
   const tags = genTags(i18n);
   return [
     {
+      title: i18n.PROJECTS.CLASSROOM.TITLE,
+      description: i18n.PROJECTS.CLASSROOM.DESCRIPTION,
+      img: ClassroomImage,
+      tags: [
+        tags.backend,
+        tags.frontend,
+        tags.nextjs,
+        tags.tailwind,
+        tags.trabajoEnEquipo,
+      ],
+      github: 'https://github.com/ynoacamino/infra-sustainable-classrooms',
+    },
+    {
+      title: i18n.PROJECTS.TECHART.TITLE,
+      description: i18n.PROJECTS.TECHART.DESCRIPTION,
+      img: TechartImage,
+      tags: [
+        tags.backend,
+        tags.frontend,
+        tags.nextjs,
+        tags.react,
+        tags.tailwind,
+        tags.motion,
+        tags.trabajoEnEquipo,
+      ],
+      github: 'https://github.com/ynoacamino/techart-pixel-canvas',
+      preview: 'https://techart.ynoacamino.me',
+    },
+    {
       title: i18n.PROJECTS.ACM.TITLE,
       description: i18n.PROJECTS.ACM.DESCRIPTION,
-      img: ACMProjectImage,
+      img: ACMImage,
       tags: [
         tags.nextjs,
         tags.react,
@@ -23,26 +54,12 @@ export const genProjects: (
         tags.trabajoEnEquipo,
       ],
       github: 'https://github.com/ynoacamino/student-chapter-acm-page',
-      preview: 'https://acm.ynoacamino.site',
-    },
-    {
-      title: i18n.PROJECTS.EXOPLANETS.TITLE,
-      description: i18n.PROJECTS.EXOPLANETS.DESCRIPTION,
-      img: ExoplanetsProjectImage,
-      tags: [
-        tags.unity,
-        tags.react,
-        tags.supabase,
-        tags.tailwind,
-        tags.frontend,
-        tags.trabajoEnEquipo,
-      ],
-      github: 'https://github.com/christianmz565/Exoplanets',
+      preview: 'https://acm.ynoacamino.me',
     },
     {
       title: i18n.PROJECTS.KNOWLEDGE_WARDENS.TITLE,
       description: i18n.PROJECTS.KNOWLEDGE_WARDENS.DESCRIPTION,
-      img: KnowledgeWardensProjectImage,
+      img: KnowledgeWardensImage,
       tags: [
         tags.nextjs,
         tags.react,
@@ -53,6 +70,20 @@ export const genProjects: (
       ],
       github: 'https://github.com/gustadev24/knowledge_wardens',
       preview: 'https://knowledge-wardens.vercel.app',
+    },
+    {
+      title: i18n.PROJECTS.EXOPLANETS.TITLE,
+      description: i18n.PROJECTS.EXOPLANETS.DESCRIPTION,
+      img: ExoplanetsImage,
+      tags: [
+        tags.unity,
+        tags.react,
+        tags.supabase,
+        tags.tailwind,
+        tags.frontend,
+        tags.trabajoEnEquipo,
+      ],
+      github: 'https://github.com/christianmz565/Exoplanets',
     },
   ];
 };
